e7ae88d4b3a314d06375be5905beb37d92099df2
Sai Aswin Raja <aswinsai45@gmail.com>
2024-11-20 22:10:00 +0530

    HTML CSS Complete

---
+++ script.js
@@ -0,0 +1,8 @@
+const passwordBox = document.getElementById("textbox");
+const length=15;
+
+const uCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+const lCase = "abcdefghijklmnopqrstuvwxyz";
+const number = "0123456789";
+const symbol = "~!@#$%^&*()_{}[]:;"
+
